ab895a0b6fa1235931983b46009678f8713036c7
Bas Straathof <bas_straathof@msn.com>
2018-10-12 12:32:22 +0200

    Update gitignore

--- jshop2-master/classpath.sh
+++ jshop2-master/classpath.sh
@@ -1,14 +1,3 @@
+#!/bin/bash
 
-CLASSPATH=.
-prepend_path()
-{
-  if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
-    eval "$1=$2:\$$1"
-  fi
-}
-
-prepend_path CLASSPATH /home/bts/Downloads/jshop2-master/bin/antlr.jar
-prepend_path CLASSPATH /home/bts/Downloads/jshop2-master/bin/JSHOP2.jar
-
-echo $CLASSPATH
-export CLASSPATH
+export CLASSPATH=~/kth/2018-2019/artificial_intelligence/assignment_3/a3/jshop2-master/antlr.jar:~/kth/2018-2019/artificial_intelligence/assignment_3/a3/jshop2-master/bin.build/JSHOP2.jar:.
