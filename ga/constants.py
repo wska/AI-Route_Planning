ebf2a045b0509ab818b51850734469d0669422a1
wska <william.skagerstrom@gmail.com>
2018-10-16 18:52:45 +0200

    Merging. Likely added some space by accident in the solve-file.

---
+++ ga/constants.py
@@ -0,0 +1,2 @@
+EDGE_LIST_PATH = "/graphs/edgeList.in"
+INITIAL_STATE_PATH = "/graphs/initState1.in"
---
+++ ga/ga.py
@@ -0,0 +1,7 @@
+import numpy as np, random, operator, pandas as pd, matplotlib.pyplot as plt
+
+class GeneticAlgorithm():
+    def __init__(self, var):
+        self.var = var
+
+    
